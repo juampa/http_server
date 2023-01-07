2decaca9a5d4f22b7e480a4a4140f3fb27dd0478
Juampa <juampa@users.noreply.github.com>
2023-01-07 11:47:56 +0100

    Accepting connections

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -13,13 +13,24 @@ impl Server {
     pub fn run(self) {
         println!("Listening on {}", self.addr);
 
-        // unwrap convierte el posible error en no recuperable. 
+        // unwrap convierte el posible error en no recuperable.
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-        loop { 
-            listener.accept() ;
-        }  
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
 
-        println!("Fin") ;
+                    // Podría ser un error si leemos más de 1024.
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received request {} ", String::from_utf8_lossy(&buffer));
+                        } 
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }                    
+                }
+                Err(e) => println!("Failed to established connection: {}", e),
+            }
+        }
     }
 }
