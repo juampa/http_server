1976b25b1fcacf2358ff4e1e9bfce553e6cca273
Juampa <juampa@users.noreply.github.com>
2023-01-07 17:31:31 +0100

    Addind lifetimes

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,4 @@
+use std::{str::FromStr, mem::uninitialized};
 pub enum Method {
     GET,
     POST,
@@ -9,3 +10,24 @@ pub enum Method {
     PATCH,
     HEAD,
 }
+
+impl  FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "DELETE" => Ok(Self::DELETE),
+            "CONNECT" => Ok(Self::CONNECT),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            "HEAD"=> Ok(Self::HEAD),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
