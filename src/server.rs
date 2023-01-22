e2b19367bdd49b712ab230fd3d73fdd11dfccd0a
Juampa <juampa@users.noreply.github.com>
2023-01-22 12:22:11 +0100

    multithread first attempt

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,11 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 
 use crate::http:: { Request, Response, StatusCode, ParseError };
+use crate::website_handler::WebsiteHandler;
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response; 
@@ -14,50 +16,59 @@ pub trait Handler {
 }
 pub struct Server {
     addr: String,
+    public_path: String
 }
 
 impl Server {
-    pub fn new(addr: String) -> Self {
-        Self { addr }
+    pub fn new(addr: String, public_path: String) -> Self {
+        Self { addr, public_path }
     }
 
-    pub fn run(self, mut handler: impl Handler) {
+    pub fn run(self) {
         println!("Listening on {}", self.addr);
 
         // unwrap convierte el posible error en no recuperable.
         let listener = TcpListener::bind(&self.addr).unwrap();
 
         loop {
-            match listener.accept() {
-                Ok((mut stream, _)) => {
+            
+            for stream in listener.incoming() {
+                let stream = stream.unwrap() ;
 
-                    // Podría ser un error si leemos más de 1024.
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Received request {} ", String::from_utf8_lossy(&buffer));
-                
-                            let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => {
-                                    handler.handle_request(&request)
-                                }
-                                Err(e) => {
-                                    handler.handle_bad_request(&e)
-                                }    
-                            };
-
-                            if let Err(e) = response.send(&mut stream) {
-                                println!("Failed to send response: {}", e);
-                            }
-                        } 
-                        Err(e) => {
-                            println!("Failed to read from connection: {}", e);
-                            
-                        }
-                    }                    
-                }
-                Err(e) => println!("Failed to established connection: {}", e),
+                thread::spawn(|| { 
+                    Self::handle_connection(stream, &self.public_path);
+                });
             }
         }
     }
+
+    fn handle_connection(mut stream: TcpStream, public_path: &String) {
+        let mut buffer = [0; 1024];
+
+        let mut handler = WebsiteHandler::new(public_path.clone());
+
+        match stream.read(&mut buffer) {
+            Ok(_) => {
+                println!("Received request {} ", String::from_utf8_lossy(&buffer));
+    
+                let response = match Request::try_from(&buffer[..]) {
+                    Ok(request) => {
+                        handler.handle_request(&request)
+                    }
+                    Err(e) => {
+                        handler.handle_bad_request(&e)
+                    }    
+                };
+
+                if let Err(e) = response.send(&mut stream) {
+                    println!("Failed to send response: {}", e);
+                }
+            } 
+            Err(e) => {
+                println!("Failed to read from connection: {}", e);
+                
+            }
+        }
+    } 
+
 }
